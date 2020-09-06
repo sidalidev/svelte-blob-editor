c401906bb692aa43d09f6ebd6aabcf5776776a2e
Sid Bentifraouine <sidbentifraouine@gmail.com>
2020-09-06 07:31:53 +0200

    Add svelte blob

---
+++ src/components/blob.svelte
@@ -0,0 +1,35 @@
+<style>
+  .eye {
+    fill: var(--ssd-white);
+    stroke: var(--ssd-black);
+    stroke-width: 4px;
+  }
+
+  .body {
+    fill: var(--ssd-purple);
+    stroke: var(--ssd-purple-light);
+    stroke-width: 2px;
+  }
+</style>
+
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 841.89 213.52">
+  <path
+    class="body"
+    d="M638.41,307.42c-5.9,45.45-13.82,53.74-30.31,57.74-15.25,3.7-24.29-6.16-29.71-30.87-9.64-44-3.88-73.65,9.64-92.9,10.71-15.25,33.73-11.31,45.8,1.42C637.17,246.34,644.18,262.92,638.41,307.42Z"
+    transform="translate(0 -155.3)" />
+
+  <ellipse
+    class="eye"
+    cx="608.78"
+    cy="259.05"
+    rx="4.84"
+    ry="3.19"
+    transform="translate(132.36 562.82) rotate(-66.99)" />
+  <ellipse
+    class="eye"
+    cx="596.61"
+    cy="254.47"
+    rx="4.84"
+    ry="3.46"
+    transform="translate(38.76 430.09) rotate(-53.78)" />
+</svg>
